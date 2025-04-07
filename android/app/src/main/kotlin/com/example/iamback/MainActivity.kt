440dc4762b79c3948b1c10950f44ca61c723edbe
crike7 <you@example.com>
2025-04-08 01:29:53 +0500

    firstday

---
+++ android/app/src/main/kotlin/com/example/iamback/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.iamback
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Back());
+}
+
+class Back extends StatelessWidget {
+  const Back({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white60,
+        appBar: AppBar(
+            title: Row(
+          children: [
+            Image.asset(
+              "assets/logo.png",
+              height: 60,
+            ),
+            const Text("MarqDev", style: TextStyle(fontSize: 25),),
+          ],
+        )),
+        body: const Column(children: [
+          Center(
+            child: Text(
+              "THIS IS A TEST",
+              style: TextStyle(fontSize: 50),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
